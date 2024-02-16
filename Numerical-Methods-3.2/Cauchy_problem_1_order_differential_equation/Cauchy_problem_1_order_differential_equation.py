ed50f9860d143687daf2662dcc1c0e6c035cce90
ByTryHard <erroraccount@bk.ru>
2024-02-16 16:29:47 +0400

    Update Cauchy_problem_1_order_differential_equation.py

--- Numerical-Methods-3.2/Cauchy_problem_1_order_differential_equation/Cauchy_problem_1_order_differential_equation.py
+++ Numerical-Methods-3.2/Cauchy_problem_1_order_differential_equation/Cauchy_problem_1_order_differential_equation.py
@@ -1,22 +1,22 @@
-g, n, m = 1, 10, 100
-
+g, n, m = 1, 10, 14
+ 
 def U(Xi, y0):
     return (m * Xi - y0) / Xi #Дифференциальное уравнение
 def Ux(Xi):
     return (m / 2) * Xi + (g / Xi) #Точное решение
-
+ 
 zmax = 0
 h = 1 / n
 y0 = (m / 2) + g #Начальное условие
-for i in range(1, n + 1):
+for i in range(n):
     Xi = 1 + i * h
     yn1 = y0 + h * U(Xi, y0)
     y0 = yn1
-    zi = abs(yn1 - U(Xi, y0))
+    zi = abs(yn1 - Ux(Xi))
     if zi > zmax:
         zmax = zi
-
+        
 print('Метод Эйлера: {:.1f}'.format(yn1))
 print('Точное решение: {:.1f}'.format(Ux(Xi)))
 print('Погрешность: {:.1f}'.format(zi))
-print('Норма: {:.1f}'.format(zmax))
+print(f'Норма: {zmax:.1f}')
