d03b00a1c7ae6b09146e9065a32f8dd6afe91bb2
ByTryHard <erroraccount@bk.ru>
2024-02-21 00:02:49 +0400

    Update Cauchy_problem_1_order_differential_equation.py

--- Numerical-Methods-3.2/Cauchy_problem_1_order_differential_equation/Cauchy_problem_1_order_differential_equation.py
+++ Numerical-Methods-3.2/Cauchy_problem_1_order_differential_equation/Cauchy_problem_1_order_differential_equation.py
@@ -20,3 +20,20 @@ print('Метод Эйлера: {:.1f}'.format(yn1))
 print('Точное решение: {:.1f}'.format(Ux(Xi)))
 print('Погрешность: {:.1f}'.format(zi))
 print(f'Норма: {zmax:.1f}')
+
+
+zmax = 0
+h = 1 / n
+y0 = (m / 2) + g #Начальное условие
+for i in range(n):
+    Xi = 1 + i * h
+    yn1 = y0 + (h / 2) * (U(Xi, y0) + U(Xi + h, y0 + h * U(Xi, y0)))
+    y0 = yn1
+    zi = abs(yn1 - Ux(Xi))
+    if zi > zmax:
+        zmax = zi
+        
+print('\nМетод Рунге-Кутты: {:.1f}'.format(yn1))
+print('Точное решение: {:.1f}'.format(Ux(Xi)))
+print('Погрешность: {:.1f}'.format(zi))
+print(f'Норма: {zmax:.1f}')
