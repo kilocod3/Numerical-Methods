e8b6957febeb369c9449a43c4d5f13b348e89904
ByTryHard <erroraccount@bk.ru>
2024-02-26 22:47:24 +0400

    Update Cauchy_problem_1_order_differential_equation.py

--- Numerical-Methods-3.2/Cauchy_problem_1_order_differential_equation/Cauchy_problem_1_order_differential_equation.py
+++ Numerical-Methods-3.2/Cauchy_problem_1_order_differential_equation/Cauchy_problem_1_order_differential_equation.py
@@ -1,39 +1,72 @@
 g, n, m = 1, 10, 14
- 
-def U(Xi, y0):
-    return (m * Xi - y0) / Xi #Дифференциальное уравнение
-def Ux(Xi):
-    return (m / 2) * Xi + (g / Xi) #Точное решение
- 
-zmax = 0
 h = 1 / n
-y0 = (m / 2) + g #Начальное условие
-for i in range(n):
-    Xi = 1 + i * h
-    yn1 = y0 + h * U(Xi, y0)
-    y0 = yn1
-    zi = abs(yn1 - Ux(Xi))
-    if zi > zmax:
-        zmax = zi
-        
-print('Метод Эйлера: {:.1f}'.format(yn1))
-print('Точное решение: {:.1f}'.format(Ux(Xi)))
-print('Погрешность: {:.1f}'.format(zi))
-print(f'Норма: {zmax:.1f}')
+def f(X, y0, m): #Дифференциальное уравнение
+    return (m * X - y0) / X
+def exact_solution(X, m, g): # Точное решение
+    return (m / 2) * X + (g / X)
+
+def euler_method(h, m, g): # Метод Эйлера
+    y0 = [(m / 2) + g]
+    for i in range(1, n + 1):
+        X = 1 + i * h
+        y0.append(y0[i-1] + h * f(X, y0[i-1], m))
+        X += h
+    return y0
+
+def runge_kutta_2(h, m, g): # Метод Рунге-Кутта 2-го порядка
+    y0 = [(m / 2) + g]
+    for i in range(1, n + 1):
+        X = 1 + i * h
+        k1 = h * f(X, y0[i-1], m)
+        k2 = h * f(X + h, y0[i-1] + k1, m)
+        y0.append(y0[i-1] + 0.5 * (k1 + k2))
+        X += h
+    return y0
+
+def runge_kutta_4(h, m, g): # Метод Рунге-Кутта 4-го порядка
+    y0 = [(m / 2) + g]
+    for i in range(1, n + 1):
+        X = 1 + i * h
+        k1 = h * f(X, y0[i-1], m)
+        k2 = h * f(X + 0.5*h, y0[i-1] + 0.5*k1, m)
+        k3 = h * f(X + 0.5*h, y0[i-1] + 0.5*k2, m)
+        k4 = h * f(X + h, y0[i-1] + k3, m)
+        y0.append(y0[i-1] + (k1 + 2*k2 + 2*k3 + k4) / 6)
+        X += h
+    return y0
+
+print('Метод Эйлера: ', euler_method(h, m, g)[-1])
+print('Метод Рунге-Кутта 2: ', runge_kutta_2(h, m, g)[-1])
+print('Метод Рунге-Кутта 4: ', runge_kutta_4(h, m, g)[-1])
+print('Точное решение: ', exact_solution(h, m, g))
 
 
-zmax = 0
-h = 1 / n
-y0 = (m / 2) + g #Начальное условие
-for i in range(n):
-    Xi = 1 + i * h
-    yn1 = y0 + (h / 2) * (U(Xi, y0) + U(Xi + h, y0 + h * U(Xi, y0)))
-    y0 = yn1
-    zi = abs(yn1 - Ux(Xi))
-    if zi > zmax:
-        zmax = zi
-        
-print('\nМетод Рунге-Кутты: {:.1f}'.format(yn1))
-print('Точное решение: {:.1f}'.format(Ux(Xi)))
-print('Погрешность: {:.1f}'.format(zi))
-print(f'Норма: {zmax:.1f}')
+def calculate_error(approximate, exact): # Функция для вычисления погрешности
+    return [abs(a - e) for a, e in zip(approximate, exact)]
+
+def max_error(errors): # Функция для нахождения максимальной погрешности
+    return max(errors)
+
+h_values = [1 / n,  1 / (n * 10)] # Параметры и шаги 10 и 100
+
+for h in h_values: #Ошибки
+    x_values = [1 + i * h for i in range(n + 1)]
+    
+    U_euler = euler_method(h, m, g) # Получение значений при различных методах
+    U_rk2 = runge_kutta_2(h, m, g)
+    U_rk4 = runge_kutta_4(h, m, g)
+    
+    exact_values = [exact_solution(X, m, g) for X in x_values] # Точное решение
+    
+    errors_euler = calculate_error(U_euler, exact_values) # Вычисление ошибок
+    errors_rk2 = calculate_error(U_rk2, exact_values)
+    errors_rk4 = calculate_error(U_rk4, exact_values)
+    
+    max_error_euler = max_error(errors_euler) # Нахождение максимальных ошибок
+    max_error_rk2 = max_error(errors_rk2)
+    max_error_rk4 = max_error(errors_rk4)
+    
+    print("Шаг h =", h) # Вывод результатов
+    print("Максимальная погрешность для метода Эйлера:", max_error_euler)
+    print("Максимальная погрешность для метода Рунге-Кутта 2-го порядка:", max_error_rk2)
+    print("Максимальная погрешность для метода Рунге-Кутта 4-го порядка:", max_error_rk4)
